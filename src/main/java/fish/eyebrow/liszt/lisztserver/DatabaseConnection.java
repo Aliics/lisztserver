a495e012810e36e75bb64557fe40455a17464e74
Nicola Paolucci <npaolucci@atlassian.com>
2018-06-27 06:40:15 +1200

    Added list content retrieval. ( Infant stage )

--- src/main/java/fish/eyebrow/liszt/lisztserver/DatabaseConnection.java
+++ src/main/java/fish/eyebrow/liszt/lisztserver/DatabaseConnection.java
@@ -2,9 +2,7 @@ package fish.eyebrow.liszt.lisztserver;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DatabaseConnection {
--- src/main/java/fish/eyebrow/liszt/lisztserver/handlers/ListRequestHandler.java
+++ src/main/java/fish/eyebrow/liszt/lisztserver/handlers/ListRequestHandler.java
@@ -21,10 +21,23 @@ public class ListRequestHandler {
 	@Produces( MediaType.APPLICATION_JSON )
 	public String clientRequestListReply( @PathParam( "listId" ) String listId ) {
 		String parsedListId = listId.split( " " )[ 0 ];
+		boolean wasListFound = doesListExist( parsedListId );
+		String contentRequested = gatherContentFromList( parsedListId );
+
+		JSONObject jsonRequestReply = new JSONObject();
+		jsonRequestReply.put( "passed-request", listId );
+		jsonRequestReply.put( "parsed-request", parsedListId );
+		jsonRequestReply.put( "parsed-request-found", wasListFound );
+		jsonRequestReply.put( "content-requested",  contentRequested );
+
+		return jsonRequestReply.toString();
+	}
+
+	private static boolean doesListExist( String listId ) {
 		boolean wasListFound = false;
 
 		try {
-			String searchForId = "SELECT * FROM lists WHERE id='" + parsedListId + "'";
+			String searchForId = "SELECT * FROM lists WHERE id='" + listId + "'";
 
 			Connection connection = DatabaseConnection.connectToDatabase( "liszt_data", "webapps/config.properties" );
 			Statement statement = connection.createStatement();
@@ -37,11 +50,26 @@ public class ListRequestHandler {
 			e.printStackTrace();
 		}
 
-		JSONObject jsonRequestReply = new JSONObject();
-		jsonRequestReply.put( "passed-request", listId );
-		jsonRequestReply.put( "parsed-request", parsedListId );
-		jsonRequestReply.put( "parsed-request-found", wasListFound );
+		return wasListFound;
+	}
 
-		return jsonRequestReply.toString();
+	private static String gatherContentFromList( String listId ) {
+		String gatheredContent = null;
+
+		try {
+			String searchForId = "SELECT content FROM lists WHERE id='" + listId + "'";
+
+			Connection connection = DatabaseConnection.connectToDatabase( "liszt_data", "webapps/config.properties" );
+			Statement statement = connection.createStatement();
+
+			ResultSet resultSet = statement.executeQuery( searchForId );
+
+			if ( resultSet.next() )
+				gatheredContent = resultSet.getString( "content" );
+		} catch ( SQLException e ) {
+			e.printStackTrace();
+		}
+
+		return gatheredContent;
 	}
 }
